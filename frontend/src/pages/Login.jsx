de726170547c9c7df682ecef943ab3f2115fb471
Luis <luispacu10@hotmail.com>
2025-06-17 08:48:23 +0200

    fix: mas traducciones

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -58,7 +58,7 @@ export const Login = () => {
         </form>
 
         <p className='text-sm text-gray-400 mt-4 text-center'>
-          ¿No tienes una cuenta? <Link to="/register" className='text-sky-400 hover:underline'>Regístrate</Link>
+          You don't have an account? <Link to="/register" className='text-sky-400 hover:underline'>Sign up</Link>
         </p>
       </div>
     </div>
